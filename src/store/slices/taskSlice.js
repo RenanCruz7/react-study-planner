1c92fddf5ad033100a725fb82a0fbac00b1d7b21
RenanCruz7 <renancruz199@gmail.com>
2025-12-09 20:55:00 -0300

    adiciona reducers para editar e deletar tarefas no slice de tarefas

--- src/store/slices/taskSlice.js
+++ src/store/slices/taskSlice.js
@@ -20,5 +20,23 @@ const taskSlice = createSlice({
       const task = state.tasks.find((task) => task.id === action.payload);
       if (task) task.completed = !task.completed;
     },
+    editTask: (state, action) => {
+      const { taskId, updatedTask } = action.payload;
+      const taskIndex = state.tasks.findIndex((task) => task.id === taskId);
+      if (taskIndex !== -1) {
+        state.tasks[taskIndex] = { ...state.tasks[taskIndex], ...updatedTask };
+      }
+    },
+    deleteTask: (state, action) => {
+      state.tasks = state.tasks.filter((task) => task.id !== action.payload);
+    }
   },
 });
+
+export const { addTask, toggleTaskComplete, editTask, deleteTask } = taskSlice.actions;
+
+export const selectTasks = (state) => state.tasks.tasks;
+export const selectPedingTasks = (state) => state.tasks.tasks.filter(task => !task.completed);
+export const selectCompletedTasks = (state) => state.tasks.tasks.filter(task => task.completed);
+
+export default taskSlice.reducer;
