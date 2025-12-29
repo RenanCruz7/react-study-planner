b089b11f27d21090b5eef35a6ee17e29361c4adb
RenanCruz7 <renancruz199@gmail.com>
2025-12-29 20:03:40 -0300

    adiciona gerenciamento de estado com localStorage e throttle para otimização

--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,9 @@ import { configureStore } from '@reduxjs/toolkit'
 import taskReducer from './slices/taskSlice'
 import themeReducer from './slices/themeSlice'
 import analyticsReducer from './slices/analyticsSlice'
+import { loadState, saveState, throttle } from './localStorage'
+
+const persistedState = loadState();
 
 export const store = configureStore({
     reducer: {
@@ -9,4 +12,18 @@ export const store = configureStore({
         theme: themeReducer,
         analytics: analyticsReducer,
     },
+    devTools: process.env.NODE_ENV !== 'production',
+    preloadedState: persistedState,
 })
+
+const throttleSaveState = throttle((state) => {
+  saveState({
+    tasks: state.tasks,
+    theme: state.theme,
+    analytics: state.analytics,
+  });
+}, 1000);
+
+store.subscribe(() => {
+    throttleSaveState(store.getState());
+});
